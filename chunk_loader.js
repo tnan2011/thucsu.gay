012ecef140e089a2ba778a272a0ba5fd8fce6891
HertaChan <135420571+Herta-Chan@users.noreply.github.com>
2023-07-05 21:46:25 +0700

    Update chunk_loader.js

--- chunk_loader.js
+++ chunk_loader.js
@@ -3,13 +3,13 @@
 window.addEventListener('DOMContentLoaded', function() {
   var container = document.getElementById('container');
   fetch(`/container.md`).then(res => res.text()).then(data => {
-    container.innerHTML = `<h1 style="padding: 50px">SỐ LẦN ĐỊT MẸ <b>HERO TEAM</b></h1>
+    container.innerHTML = `<h1 style="padding: 50px">SỐ LẦN ĐỊT MẸ <b>RGR</b></h1>
 <!-- Start of CuterCounter Code -->
 <a href="#" target="_blank"><img src="https://www.cutercounter.com/hits.php?id=hxfakxd&nd=7&style=20" border="0"
     alt="hit counter"></a>
 <!-- End of CuterCounter Code -->
-<h1 style="padding: 50px 30px">ĐỊT MẸ <b>HERO TEAM</b></h1>
-`+ window.hiddenCache + data + container.innerHTML + `<p style="font-size:11px;margin:1rem" aria-hidden="true"> địt mẹ hero team, hẻo team rác, con chó hẻo
+<h1 style="padding: 50px 30px">ĐỊT MẸ <b>RGR</b></h1>
+`+ window.hiddenCache + data + container.innerHTML + `<p style="font-size:11px;margin:1rem" aria-hidden="true"> rgr ngu vcl
 </p>`;
 
   })
